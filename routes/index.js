8ba10752fce35b9716dfbc4df315dcbc066bd1f5
HermesKeng <kaining1101@gmail.com>
2021-03-04 10:30:59 -0800

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
 /*
  * GET home page.
  */
-var labs = require('../labs.json');
+var labs = require("../labs.json");
 
-exports.view = function(req, res){
-  if(labs.length < 9) {
-  	var todaysLab = [{
-		"id": "lab-9",
-		"title": "Debugging 101",
-		"date": "March 7",
-		"image": "debug.png"
-	}];
+exports.view = function (req, res) {
+  if (labs.length < 9) {
+    var todaysLab = {
+      id: "lab-9",
+      title: "Debugging 101",
+      date: "March 7",
+      image: "debug.png",
+    };
 
-  	labs.push(todaysLab);
+    labs.push(todaysLab);
+    console.log(labs);
   }
 
-  res.render('index', {
-	'labs': labs
+  res.render("index", {
+    labs: labs,
   });
 };
